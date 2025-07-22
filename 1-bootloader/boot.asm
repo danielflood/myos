3338dcf95ec3e2c2bc3c972f8745526e5188ce09
danielflood <dandan.flood@gmail.com>
2025-07-22 13:29:40 +0700

    Updated bootloader to use VGA instead of interrupts

--- 1-bootloader/boot.asm
+++ 1-bootloader/boot.asm
@@ -3,13 +3,28 @@ org 0x7C00                ; BIOS loads us here
 
 start:
     mov si, msg
+    mov ax, 0xB800       ; VGA Memory first 16bits
+    mov es, ax           ; Have to load into ES from a general purpose register e.g. AX   
+    mov bx, 0x0000       ; Second 16 bits
+    mov ah, 0x07         ; Light grey on black?
+    mov al, 0x20         ; Blank space for clearing the screen
+
+.clear_screen:
+    cmp bx, 0xFA0
+    jz .reset_screen
+    mov word [es:bx], ax
+    add bx, 2
+    jmp .clear_screen
+
+.reset_screen:
+    mov bx, 0x0000       ; Reset BX to point at the start of the VGA buffer
 
 .print_char:
     lodsb                 ; Load next byte from [SI] into AL
     or al, al             ; Check for null terminator
     jz .hang
-    mov ah, 0x0E          ; Teletype BIOS function
-    int 0x10              ; Call BIOS to print AL
+    mov word [es:bx], ax
+    add bx, 2
     jmp .print_char
 
 .hang:
